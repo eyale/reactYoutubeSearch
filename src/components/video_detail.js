3386420d1e909cf646d2b3753ccab873f6997ee0
Anton Honcharov <4eyalush@gmail.com>
2017-02-26 19:58:29 +0200

    adding the styles

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -9,9 +9,9 @@ const VideoDetail = ({video}) => {
         videoId = video.id.videoId,
         url = `https://www.youtube.com/embed/${videoId}`;
 
-        
+
   return(
-    <div className='video_detail col-md-8'>
+    <div className='video-detail col-md-8'>
       <div className='embed-responsive embed-responsive-16by9'>
         <iframe
           className='embed-responsive-item'
